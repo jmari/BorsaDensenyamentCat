22f769be057b03dc48f8c76798e4dea1023c2339
Víctor <vboix2@gmail.com>
2019-03-22 13:05:43 +0100

    Update __get_links

--- src/scraper.py
+++ src/scraper.py
@@ -1,13 +1,13 @@
 import requests
 import re
+import csv
 from bs4 import BeautifulSoup
 
 
-class WebScraper():
+class WebScraper:
 
     def __init__(self):
         self.url = "http://sindicat.net/borsa/"
-        
 
     def __download(self, url):
         print("Downloading", url, "...")
@@ -15,48 +15,28 @@ class WebScraper():
 
         return r.text
 
-    def __get_links_new(self, html):
-        # captura els enllaços a partir del 2015
-        bs = BeautifulSoup(html, "html.parser")
-        nodes_a = bs.find('h3').find_all('a')
-
-        links = []
-        for node in nodes_a:
-            if node.text != "cat":
-                links.append(node['href'])
-
-        return links
-
-    def __get_links_old(self, html):
-        # captura els enllaços anteriors al 2015
+    def __get_links(self, html):
         bs = BeautifulSoup(html, "html.parser")
         nodes_a = bs.find_all('a')
        
         links = []
         for node in nodes_a:
-            if node.text != "cat" and '/ctot.php?e' in node['href']:
+            if '/ctot.php?e' in node['href']:
                 links.append(node['href'])
 
         return links
 
-
     def scrape(self, course):
         html = self.__download(self.url + course)
-        links = []
-
-        if course in ["1314","1415"]:
-            links = self.__get_links_old(html)
-        else:
-            links = self.__get_links_new(html)
-
+        links = self.__get_links(html)
 
         for link in links[0:1]:  # Per capturar tots els enllaços treure l'slicing
             html = self.__download(link)
             bs = BeautifulSoup(html, "html.parser")
 
+            # STT and ESP
             titles = bs.find_all('h1')
             title = titles[1].text.split("-")
-
             p = re.compile('SSTT|ESPECIALITAT|:|\s')
             sstt = p.sub('', title[0])
             esp = p.sub('', title[1])
