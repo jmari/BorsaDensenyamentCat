35405bba1cd43b1a3f6c491a7bf184e35249d292
jmari <jmariaguirre@gmail.com>
2019-03-31 12:58:47 +0200

    df.to_csv guardat a l'arxiu

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
 from scraper import WebScraper
+from pandas import DataFrame
+import os
 
 ws = WebScraper()
 
 # Indicar el curs a capturar entre 1314 i 1819
-course = "1718"
-course_name = "../data/" + "dades" + course + ".csv"
+course = "1819"
+
+outname = "dades" + course + ".csv"
+
+outdir = './data'
+if not os.path.exists(outdir):
+    os.mkdir(outdir)
+course_file = os.path.join(outdir, outname)    
 
 df = ws.scrape(course)
-df.to_csv(index=False)
+df.to_csv(course_file, index=False,header=True,sep=';')
